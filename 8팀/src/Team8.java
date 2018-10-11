265c982ab1ab0c013bff63f09c1f5388c90cc35d
Kimjeongwoon <rlawjddns0@naver.com>
2018-10-11 19:25:51 +0900

    ada

--- 8팀/src/Team8.java
+++ 8팀/src/Team8.java
@@ -4,14 +4,13 @@ public class Team8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
+
 		System.out.println("qdqdqdqd");
 		System.out.println("faffaffa");
-=======
 		System.out.println("병합 한드아");
 	
 		System.out.println("adadada");
->>>>>>> refs/heads/master
+
 	}
 
 }
